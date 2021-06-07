f055eb5b006231e38500cb6dc4e6c5faf0993d8d
ponxpon <ponponpon816@gmail.com>
2021-06-07 20:52:06 +0000

    [Add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'brands/index'
-  get 'items/index'
-  get 'items/new'
-  get 'items/show'
-  get 'items/edit'
-  get 'items/graph'
-  get 'users/index'
-  get 'users/unsubscribe'
-  get 'homes/top'
+  root to: 'homes#top'
   get 'homes/about'
   get 'homes/contact'
+  
   devise_for :users
+  
+  resources :items
+  get 'items/graph'
+  resources :brands, only: [:index, :create, :update]
+  resources :categories, only: [:index, :create, :update]
+  resources :users, only: [:index, :update]
+  get 'users/unsubscribe'
+  patch 'users/withdraw'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
