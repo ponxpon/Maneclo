a2ae532b329ed7f03deac35a00e60dd27d5dc8ea
ponxpon <ponponpon816@gmail.com>
2021-06-21 09:40:03 +0000

    [Update]views/homes,items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,21 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  
+
+  # サインイン後itemsに遷移
+  def after_sign_in_path_for(resource)
+    items_path
+  end
+
+  # サインアップ後itemsに遷移
+  def after_sign_up_path_for(resource)
+    customer_path(current_customer)
+  end
+
    # 会員の論理削除のための記述。退会後は同じアカウントでは利用できない。
   def reject_user
     @user = User.find_by(name: params[:user][:name])
-    if @user 
+    if @user
       if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == false)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to new_user_registration
